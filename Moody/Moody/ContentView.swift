760ba78987c81973473925ce96a325dac993c893
Tornike Parunashvili <parunashvilitornike@gmail.com>
2024-11-07 14:06:18 +0400

    Created SwiftUI Project

---
+++ Moody/Moody/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Moody
+//
+//  Created by tornike <parunashvili on 07.11.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Moody/Moody/MoodyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodyApp.swift
+//  Moody
+//
+//  Created by tornike <parunashvili on 07.11.24.
+//
+
+import SwiftUI
+
+@main
+struct MoodyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
